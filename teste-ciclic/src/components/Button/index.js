8029f16e64ba6b73349363df36c278d70bdca2c1
wps13 <willanepaiva@hotmail.com>
2019-03-04 09:44:06 -0300

    Add styled components(Button and Box)

---
+++ teste-ciclic/src/components/Button/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: #2f1e9e;
+  border: 1px #1f1756 solid;
+  &:hover {
+    background: #6c63a5;
+  }
+`;
+
+export default Button;
