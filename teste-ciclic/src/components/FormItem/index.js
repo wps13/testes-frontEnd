5446695bf2f1b9704b3293d631c0c0cd53fe3ffb
wps13 <willanepaiva@hotmail.com>
2019-03-04 10:01:12 -0300

    Add new components(FormItem & OuterBox)

---
+++ teste-ciclic/src/components/FormItem/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const FormItem = styled.div`
+  margin: 5px 0 5px 0;
+  display: flex;
+  width: 100%;
+  input,
+  select {
+    margin-left: 5px;
+    width: 100%;
+  }
+`;
+
+export default FormItem;
