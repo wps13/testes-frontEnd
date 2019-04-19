e2cac241a5e5298f5b0d0ab02490e3ec91426b90
wps13 <willanepaiva@hotmail.com>
2019-04-19 11:02:01 -0300

    Integrate redux to the app in ciclic

--- ciclic/src/App.js
+++ ciclic/src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
 import FormSimulation from "./containers/FormSimulation";
+
+import reducer from "./store/ducks/simulation";
+
+import "./App.css";
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
   render() {
-    return <FormSimulation />;
+    return (
+      <Provider store={store}>
+        <FormSimulation />
+      </Provider>
+    );
   }
 }
 
