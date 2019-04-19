6b6c3bfc108a3d5572f443baff118bbfb9bb37e7
wps13 <willanepaiva@hotmail.com>
2019-04-19 10:24:25 -0300

    Modify ciclic/FormItem style

--- ciclic/src/components/FormItem/index.js
+++ ciclic/src/components/FormItem/index.js
@@ -4,10 +4,17 @@ const FormItem = styled.div`
   margin: 5px 0 5px 0;
   display: flex;
   width: 100%;
-  input,
+  input {
+    margin-left: 5px;
+    width: 100%;
+    border-radius: 5px;
+    background: #fff;
+  }
   select {
     margin-left: 5px;
     width: 100%;
+    border-radius: 2px;
+    background: #fff;
   }
 `;
 
