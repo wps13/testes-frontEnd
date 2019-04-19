bc758177987969829af67b423d01f4f76e5d3008
wps13 <willanepaiva@hotmail.com>
2019-04-19 14:45:09 -0300

    add routes to ciclic/App

--- ciclic/src/App.js
+++ ciclic/src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-
-import FormSimulation from "./containers/FormSimulation";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
 
 import reducer from "./store/ducks/simulation";
+import sagas from "./store/sagas/simulation";
 
 import "./App.css";
+import Routes from "./routes";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(sagaMiddleware))
 );
+
+sagaMiddleware.run(sagas);
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <FormSimulation />
+        <Routes />
       </Provider>
     );
   }
