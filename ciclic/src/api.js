ae42e7b76e7be88c49a9ef9493fc2e37b81c64f3
wps13 <willanepaiva@hotmail.com>
2019-04-19 14:46:35 -0300

    modify base url in ciclic/api

--- ciclic/src/api.js
+++ ciclic/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/http://api.mathjs.org/v4/",
+  baseURL: "http://api.mathjs.org/v4/",
   crossDomain: true
 });
 
