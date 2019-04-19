fddf86ef9209e7fdb0c8a7b5df5e6fab7d55581d
wps13 <willanepaiva@hotmail.com>
2019-04-19 09:36:36 -0300

    Change ciclic/Button

--- ciclic/src/components/Button/index.js
+++ ciclic/src/components/Button/index.js
@@ -2,10 +2,15 @@ import styled from "styled-components";
 
 const Button = styled.button`
   background: #594ea0;
-  border: 1px #1f1756 solid;
+  border: 0.5px #1f1756 solid;
+  border-radius: 2px;
   color: white;
+  font-size: 18px;
+  padding: 5px;
+  margin: 5px;
   &:hover {
     background: #9188ce;
+    cursor: pointer;
   }
 `;
 
