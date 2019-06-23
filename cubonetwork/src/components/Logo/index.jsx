5641e8f740dcc2e1550af75feabf4b0267bb906d
Willane Paiva <willanepaiva@hotmail.com>
2019-06-23 19:42:46 -0300

    Add cubonetwork/logo

---
+++ cubonetwork/src/components/Logo/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./style.scss";
+
+const Logo = () => {
+  return (
+    <div className="logo-header">
+      <p className="logo-header-product">Cubo</p>
+      <div className="logo-header-info-company">
+        <span className="logo-header-info">coworking</span>
+        <span className="logo-header-company">Itaú</span>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
