a37d5f02978b914782aee5164acda6eedada606a
Willane Paiva <willanepaiva@hotmail.com>
2019-06-23 19:44:00 -0300

    Add cubonetwork/fake data and icons for services

---
+++ cubonetwork/src/utils/fakeData.js
@@ -0,0 +1,28 @@
+import { Piggy, Accounting, IncreaseMoney, Creditcard } from "../utils/icons";
+
+export const services = () => [
+  {
+    title: "Serviço 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: <Piggy />
+  },
+  {
+    title: "Serviço 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: <Accounting />
+  },
+  {
+    title: "Serviço 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: <Creditcard />
+  },
+  {
+    title: "Serviço 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    image: <IncreaseMoney />
+  }
+];
