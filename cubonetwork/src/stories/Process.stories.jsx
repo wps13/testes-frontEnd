e6c036dce585d12c7606bad28b6567a4db5f47d1
Willane Paiva <willanepaiva@hotmail.com>
2019-07-21 16:31:40 -0300

    Add stories for process

---
+++ cubonetwork/src/stories/Process.stories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import ProcessStep from "../components/ProcessStep";
+import ProcessArea from "../components/ProcessArea";
+import ArrowCurved from "../components/ArrowCurved";
+
+import { Globe } from "../utils/icons";
+
+storiesOf("Process", module)
+  .add("Process Item", () => (
+    <ProcessStep
+      Icon={Globe}
+      title="Passo 1"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    />
+  ))
+  .add("All Process", () => <ProcessArea />)
+  .add("Arrow Down", () => <ArrowCurved positionClass="down" />)
+  .add("Arrow Up", () => <ArrowCurved positionClass="up" />);
