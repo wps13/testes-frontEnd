5ab201498930b72ae57d1dbfe9ee661bcbac1a9d
Willane Paiva <willanepaiva@hotmail.com>
2019-07-28 14:58:06 -0300

    Improve sign up area

--- cubonetwork/src/components/SignUpForm/index.jsx
+++ cubonetwork/src/components/SignUpForm/index.jsx
@@ -4,16 +4,31 @@ import FormInput from "../FormInput";
 
 import "./style.scss";
 
-const SignUp = () => {
+const SignUp = ({ inputChange, btnClick }) => {
   return (
     <div className="sign-up">
       <h1>Cadastre-se</h1>
       <p>Alguma descrição aqui</p>
       <div className="sign-up-form">
-        <FormInput title="Nome" dataType="text" />
-        <FormInput title="Email" dataType="email" />
-        <FormInput title="Telefone" dataType="tel" />
-        <button>Enviar</button>
+        <FormInput
+          title="Nome"
+          dataType="text"
+          posClass="first"
+          change={e => inputChange(e)}
+        />
+        <FormInput
+          title="Email"
+          dataType="email"
+          posClass="second"
+          change={e => inputChange(e)}
+        />
+        <FormInput
+          title="Telefone"
+          dataType="tel"
+          posClass="third"
+          change={e => inputChange(e)}
+        />
+        <button onClick={() => btnClick()}>Enviar</button>
       </div>
     </div>
   );
