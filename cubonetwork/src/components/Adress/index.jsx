ee0706dbe81ebea64113c459f85d10b6091ac103
Willane Paiva <willanepaiva@hotmail.com>
2019-07-27 22:55:55 -0300

    Add adress component

---
+++ cubonetwork/src/components/Adress/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./style.scss";
+
+const Adress = () => {
+  return (
+    <div className="adress">
+      <h3 className="adress-title">cubo</h3>
+      <p className="adress-street">Rua dos bobos, 0</p>
+      <p className="adress-others">00000-000, Baixada</p>
+    </div>
+  );
+};
+
+export default Adress;
