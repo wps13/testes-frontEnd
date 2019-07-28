e1c6bcf30e3961088e8d2477b2ef4fdbe0c523ae
Willane Paiva <willanepaiva@hotmail.com>
2019-07-27 22:54:56 -0300

    Add copyright bar

---
+++ cubonetwork/src/components/Copyright/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+const Copyright = () => {
+  return (
+    <div className="copyright">
+      <p>Copyright 2019</p>
+    </div>
+  );
+};
+
+export default Copyright;
