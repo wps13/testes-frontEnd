6da865cfedcc9fecc35df712a4c0b8591cec2125
Willane Paiva <willanepaiva@hotmail.com>
2019-09-14 22:29:32 -0300

    Refactor home

--- cubonetwork/src/containers/Home/index.jsx
+++ cubonetwork/src/containers/Home/index.jsx
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
 
-import Logo from "../../components/Logo";
-import MenuBar from "../../components/MenuBar";
-import ServicesArea from "../../components/ServicesArea";
-import ProcessArea from "../../components/ProcessArea";
-import SignUpForm from "../../components/SignUpForm";
-import Adress from "../../components/Adress";
-import SNSArea from "../../components/SNSArea";
-import Copyright from "../../components/Copyright";
-import About from "../../components/About";
-import Table from "../../components/Table";
+import UserDataSection from "../../components/UserDataSection";
+import FooterSection from "../../components/FooterSection";
+import ServicesSection from "../../components/ServicesSection";
+import AboutSection from "../../components/AboutSection";
+import StepsSection from "../../components/StepsSection";
+import MainSection from "../../components/MainSection";
+import SignUpSection from "../../components/SignUpSection";
 
 import "./style.scss";
 
-const fakeData = [
-  {
-    id: 1,
-    name: "Joao",
-    lastName: "Silva",
-    value: 9000
-  },
-  {
-    id: 2,
-    name: "Joao",
-    lastName: "Silva",
-    value: 9000
-  }
-];
+import { userData } from "../../utils/fakeData";
+import HeaderSection from "../../components/HeaderSection";
 
 const Home = () => {
   const [inputs, onChangeInput] = useState({});
@@ -37,56 +22,14 @@ const Home = () => {
 
   return (
     <div className="home">
-      <header className="home-header">
-        <Logo />
-        <MenuBar />
-      </header>
-      <main className="home-main">
-        <h2 style={{ textAlign: "center" }}>Carrossel a construir</h2>
-      </main>
-      <section className="home-services">
-        <div className="home-services-container">
-          <div className="home-services-container-title">
-            <h2>Serviços</h2>
-            <p>Alguma descrição aqui</p>
-          </div>
-          <ServicesArea />
-        </div>
-      </section>
-      <section className="home-about">
-        <div className="home-about-container">
-          <About />
-        </div>
-      </section>
-      <section className="home-steps">
-        <div className="home-steps-container">
-          <div className="home-steps-container-title">
-            <h2>Como funciona?</h2>
-            <p>alguma desc</p>
-          </div>
-
-          <ProcessArea />
-        </div>
-      </section>
-      <section className="home-signup">
-        <SignUpForm inputChange={handleInput} btnClick={sendData} />
-      </section>
-      <section className="home-graph">
-        <div className="home-graph-container">
-          <h2>Dados</h2>
-          <p>Gráfico</p>
-          <Table data={fakeData} />
-        </div>
-      </section>
-      <footer className="home-footer">
-        <div className="home-footer-container">
-          <div className="home-footer-container-sns">
-            <Adress />
-            <SNSArea />
-          </div>
-          <Copyright />
-        </div>
-      </footer>
+      <HeaderSection />
+      <MainSection />
+      <ServicesSection />
+      <AboutSection />
+      <StepsSection />
+      <SignUpSection inputChange={handleInput} btnClick={sendData} />
+      <UserDataSection data={userData} />
+      <FooterSection />
     </div>
   );
 };
