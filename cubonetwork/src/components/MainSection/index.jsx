227beaadb51cc831585d9f2c1f79646604b15110
Willane Paiva <willanepaiva@hotmail.com>
2019-09-14 22:26:17 -0300

    Add main section

---
+++ cubonetwork/src/components/MainSection/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.scss";
+
+const MainSection = () => {
+  return (
+    <main className="home-main">
+      <h2 style={{ textAlign: "center" }}>Carrossel a construir</h2>
+    </main>
+  );
+};
+
+export default MainSection;
