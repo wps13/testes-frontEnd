cc6265f11431e7aa28ffb19919bcebc03e600b38
Willane Paiva <willanepaiva@hotmail.com>
2019-09-14 22:29:15 -0300

    add fake data to graph and table

--- cubonetwork/src/utils/fakeData.js
+++ cubonetwork/src/utils/fakeData.js
@@ -27,3 +27,51 @@ export const services = () => [
     image: <IncreaseMoney />
   }
 ];
+
+export const carouselData = [
+  {
+    title: " 1",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    order: "first"
+  },
+  {
+    title: " 2",
+    text:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    order: "second"
+  }
+];
+
+export const userData = [
+  {
+    id: 1,
+    name: "Joao",
+    lastName: "Silva",
+    value: 25
+  },
+  {
+    id: 2,
+    name: "Joao",
+    lastName: "Melo",
+    value: 20
+  },
+  {
+    id: 3,
+    name: " Maria",
+    lastName: "Pinheiros",
+    value: 30
+  },
+  {
+    id: 4,
+    name: " Maria",
+    lastName: "da Silva",
+    value: 5
+  },
+  {
+    id: 5,
+    name: " Zé",
+    lastName: "Maria",
+    value: 20
+  }
+];
