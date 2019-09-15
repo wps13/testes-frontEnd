d5edf2ca19bc6f0790378ea5dfca5f0a17956cd7
Willane Paiva <willanepaiva@hotmail.com>
2019-09-14 22:28:05 -0300

    Add user data section

---
+++ cubonetwork/src/components/UserDataSection/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Table from "../Table";
+import UserGraph from "../UserGraph";
+
+import "./style.scss";
+
+const UserDataSection = ({ data }) => {
+  return (
+    <div className="home-graph-container">
+      <div className="home-graph__title">
+        <h2>Dados</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor..
+        </p>
+      </div>
+      <div className="home-graph__data">
+        <Table data={data} />
+        <UserGraph userData={data} />
+      </div>
+    </div>
+  );
+};
+
+export default UserDataSection;
