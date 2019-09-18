903bb329be258cae712f06beded2d51ae4920cd4
Willane Paiva <willanepaiva@hotmail.com>
2019-09-17 22:17:13 -0300

    Add more functions to format data

--- cubonetwork/src/utils/helpFunctions.js
+++ cubonetwork/src/utils/helpFunctions.js
@@ -16,3 +16,44 @@ export const formatMoneyString = value => `R$ ${value}`;
 export const formatHourString = value => `${value}:00`;
 
 export const formatPercentageString = value => `${value}%`;
+
+export const complementaryHour = value => 24 - value;
+
+export const complementaryPercentage = value =>
+  value < 100 ? 100 - value : value - 100;
+
+export const complementaryMoney = value =>
+  value < 1000 ? 1000 - value : value - 1000;
+
+export const formatDataAbout = (value, type) => {
+  let formatedValue, complementaryValue;
+
+  switch (type) {
+    case "time":
+      formatedValue = formatHourString(value);
+      complementaryValue = complementaryHour(value);
+      break;
+    case "money":
+      formatedValue = formatMoneyString(value);
+      complementaryValue = complementaryMoney(value);
+      break;
+    case "percentage":
+      formatedValue = formatPercentageString(value);
+      complementaryValue = complementaryPercentage(value);
+      break;
+    default:
+      formatedValue = "";
+      complementaryValue = "";
+      break;
+  }
+
+  console.log([
+    { value, name: formatedValue },
+    { value: complementaryValue, name: "" }
+  ]);
+
+  return [
+    { value, name: formatedValue },
+    { value: complementaryValue, name: "" }
+  ];
+};
